4f632cdd6fc1643fe3c31067ad82158437dd7deb
pipekl <79262323423@yandex.ru>
2021-04-09 18:38:26 +0300

    Start app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sound.midi.Soundbank;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorld1 == helloWorld2);
+        System.out.println(cat1 == cat2);
     }
 }
